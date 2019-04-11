1716b7dd4ec159f9d4e97f77a06931db82cda938
Kyle Kamperschroer <kykamper@microsoft.com>
2019-04-11 15:05:44 -0500

    Fix AndroidManifest.xml to actually display push notifications

--- samples/notification-hubs-test-app/app/src/main/java/com/microsoft/notification_hubs_test_app/DemoNotificationsHandler.java
+++ samples/notification-hubs-test-app/app/src/main/java/com/microsoft/notification_hubs_test_app/DemoNotificationsHandler.java
@@ -1,14 +1,7 @@
 package com.microsoft.notification_hubs_test_app;
 
-import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.RemoteMessage;
 import com.microsoft.windowsazure.notifications.NotificationsHandler;
