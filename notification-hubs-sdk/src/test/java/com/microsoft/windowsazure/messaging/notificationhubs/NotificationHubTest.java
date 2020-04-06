0f82b01018168949761a0f6e2d8f3483801ebc9b
Martin Strobel <marstr@microsoft.com>
2020-04-06 15:59:27 -0700

    Adding Middleware test

--- notification-hubs-sdk/src/test/java/com/microsoft/windowsazure/messaging/notificationhubs/NotificationHubTest.java
+++ notification-hubs-sdk/src/test/java/com/microsoft/windowsazure/messaging/notificationhubs/NotificationHubTest.java
@@ -1,14 +1,12 @@
 package com.microsoft.windowsazure.messaging.notificationhubs;
 
+import android.app.Activity;
+import android.os.Bundle;
+import com.google.firebase.messaging.RemoteMessage;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class NotificationHubTest {
-
-    @Test
-    public void setInstanceListener() {
-    }
 
     @Test
     public void useInstanceMiddleware() {
@@ -29,5 +27,41 @@ public class NotificationHubTest {
 
         assertEquals("Middleware should have been invoked exactly once.", 1, callCount[0]);
         assertEquals("InstallationEnricher should have been invoked exactly once.", 1, callCount[1]);
+    }
+
+    @Test
+    public void testUseInstanceMiddleware() {
+        NotificationHub specimen = new NotificationHub();
+
+        final int[] callCount = new int[]{0, 0, 0, 0};
+
+        specimen.useInstanceMiddleware(next -> {
+            assertEquals("Middleware should be called as a stack", 1, callCount[2]);
+            callCount[0]++;
+            return subject -> {
+                assertEquals("Installation Enrichers are called in the order the middleware was added", 0, callCount[3]);
+                callCount[1]++;
+                next.enrichInstallation(subject);
+            };
+        });
+
+        specimen.useInstanceMiddleware(next -> {
+            assertEquals("Middleware should be called as a stack", 0, callCount[0]);
+            callCount[2]++;
+            return new InstallationEnricher() {
+                @Override
+                public void enrichInstallation(Installation subject) {
+                    assertEquals("Installation Enrichers are called in the order the middleware was added", 1, callCount[1]);
+                    callCount[3]++;
+                    next.enrichInstallation(subject);
+                }
+            };
+        });
+
+        specimen.reinstallInstance();
+
+        for (int x: callCount) {
+            assertEquals("each method should be called exactly once", 1, x);
+        }
     }
 }
