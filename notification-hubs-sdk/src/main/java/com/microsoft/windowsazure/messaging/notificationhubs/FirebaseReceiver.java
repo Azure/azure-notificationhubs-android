b031eb29bb44f8dc56a874edad774da2b3214422
Martin Strobel <marstr@microsoft.com>
2020-04-07 15:19:43 -0700

    Isolating all Firebase specific logic into a single class.

--- notification-hubs-sdk/src/main/java/com/microsoft/windowsazure/messaging/notificationhubs/FirebaseReceiver.java
+++ notification-hubs-sdk/src/main/java/com/microsoft/windowsazure/messaging/notificationhubs/FirebaseReceiver.java
@@ -4,7 +4,7 @@ import androidx.annotation.NonNull;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-public final class NotificationReceiver extends FirebaseMessagingService {
+public final class FirebaseReceiver extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         NotificationHub.relayMessage(remoteMessage);
@@ -13,5 +13,18 @@ public final class NotificationReceiver extends FirebaseMessagingService {
     @Override
     public void onNewToken(@NonNull String s) {
         NotificationHub.setPushChannel(s);
+    }
+
+    /**
+     * Converts from a {@link RemoteMessage} to a {@link NotificationMessage}.
+     * @param remoteMessage The message intended for this device, as delivered by Firebase.
+     * @return A fully instantiated {@link NotificationMessage}.
+     */
+    static NotificationMessage getNotificationMessage(RemoteMessage remoteMessage) {
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        return new NotificationMessage(
+                notification.getTitle(),
+                notification.getBody(),
+                remoteMessage.getData());
     }
 }
