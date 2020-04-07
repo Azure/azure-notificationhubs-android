b5039971c5e48447912617bb42561b6e3cdc88c1
Martin Strobel <marstr@microsoft.com>
2020-04-07 10:16:22 -0700

    Adding documentation for Installation manager.

--- notification-hubs-sdk/src/main/java/com/microsoft/windowsazure/messaging/notificationhubs/InstallationManager.java
+++ notification-hubs-sdk/src/main/java/com/microsoft/windowsazure/messaging/notificationhubs/InstallationManager.java
@@ -3,5 +3,10 @@ package com.microsoft.windowsazure.messaging.notificationhubs;
 import com.microsoft.windowsazure.messaging.notificationhubs.async.NotificationHubFuture;
 
 public interface InstallationManager {
+    /**
+     * Updates a backend with the updated Installation information for this device.
+     * @param installation The record to update.
+     * @return A future, with the Installation ID as the value.
+     */
     NotificationHubFuture<String> saveInstallation(Installation installation);
 }
