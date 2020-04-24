f73b66d6615b256c10400b3471cadf21b92c2c25
Polina Baranova <polina.baranova@akvelon.com>
2020-04-24 13:12:39 +0300

    Resolving conflict

--- notification-hubs-sdk/src/main/java/com/microsoft/windowsazure/messaging/notificationhubs/InstallationTemplate.java
+++ notification-hubs-sdk/src/main/java/com/microsoft/windowsazure/messaging/notificationhubs/InstallationTemplate.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 import androidx.annotation.Nullable;
