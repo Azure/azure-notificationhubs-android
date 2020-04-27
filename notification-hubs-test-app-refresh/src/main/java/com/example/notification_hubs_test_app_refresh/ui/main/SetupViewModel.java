628994f68b81c8a8a6d6fbe17223040342700be0
Martin Strobel <marstr@microsoft.com>
2020-04-27 11:28:00 -0700

    Pulling InstallationId setting out of visitor.
    In general, I'm trying to get rid of the need to call
    `NotificationHub.reinstall()` to fetch information about your
    environment. This change should initialize InstallationId correctly
    without the need for the reinstall.

--- notification-hubs-test-app-refresh/src/main/java/com/example/notification_hubs_test_app_refresh/ui/main/SetupViewModel.java
+++ notification-hubs-test-app-refresh/src/main/java/com/example/notification_hubs_test_app_refresh/ui/main/SetupViewModel.java
@@ -20,17 +20,6 @@ public class SetupViewModel extends ViewModel {
     private final MutableLiveData<List<String>> mTags = new MutableLiveData<List<String>>();
 
     public SetupViewModel() {
-        NotificationHub.useVisitor(
-            // TODO: Instead of having this be InstallationMiddleware, which will get a partially
-            //       hydrated Installation, have this be an InstallationManager that intercepts the
-            //       call with the finalized installation.
-            subject -> {
-                String installationId = subject.getInstallationId();
-                if (installationId == null) {
-                    installationId = getUnknownText();
-                }
-                mInstallationId.setValue(installationId);
-            });
         mTags.setValue(iterableToList(NotificationHub.getTags()));
 
         String pushChannel = NotificationHub.getPushChannel();
@@ -38,14 +27,10 @@ public class SetupViewModel extends ViewModel {
             mDeviceToken.setValue(pushChannel);
         }
 
-        // TODO: This reinstall is forced to take advantage of the hook we setup above into the
-        //       Installation creation process. Honestly, this stinks. We shouldn't encourage people
-        //       to reinstall the app every time this screen is launched.
-        //       Because this is a sample application, we probably need to either decide to remove
-        //       the device token/installation id fields on the setup fragment, or add getters for
-        //       this information. This decisions will hinge on how common of scenario it is to
-        //       fetch your installation id or device token.
-        NotificationHub.reinstall();
+        String installationId = NotificationHub.getInstallationId();
+        if (installationId != null) {
+            mInstallationId.setValue(installationId);
+        }
     }
 
     public LiveData<String> getDeviceToken() {
