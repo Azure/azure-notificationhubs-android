653edf232f3ed23d530f72d5bca2d280d25fb8db
Martin Strobel <marstr@microsoft.com>
2020-04-30 08:44:12 -0700

    Documenting FirebaseReceiver

--- notification-hubs-sdk/src/main/java/com/microsoft/windowsazure/messaging/notificationhubs/FirebaseReceiver.java
+++ notification-hubs-sdk/src/main/java/com/microsoft/windowsazure/messaging/notificationhubs/FirebaseReceiver.java
@@ -8,8 +8,18 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+/**
+ * Facilitates processing messages that are delivered to the device via Firebase Cloud Messaging
+ * (FCM).
+ *
+ * This class must be public to be invoked by Android. However, there should be no need for
+ * consumers to interact directly with this class.
+ */
 public final class FirebaseReceiver extends FirebaseMessagingService {
 
+    /**
+     * Loads all resources that the service will need to execute.
+     */
     @Override
     public void onCreate() {
         super.onCreate();
@@ -28,11 +38,20 @@ public final class FirebaseReceiver extends FirebaseMessagingService {
         }
     }
 
+    /**
+     * Callback for the system when a new {@link RemoteMessage} is received.
+     * @param remoteMessage the newly acquired message.
+     */
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         NotificationHub.relayMessage(getNotificationMessage(remoteMessage));
     }
 
+    /**
+     * Callback for when Firebase assigns a new unique identifier for pushing notifications to this
+     * application on this device.
+     * @param s The new unique identifier for this application.
+     */
     @Override
     public void onNewToken(@NonNull String s) {
         NotificationHub.setPushChannel(s);
