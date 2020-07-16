1569f43c006d3a1545914dcf31ecfb45295f378c
Martin Strobel <marstr@microsoft.com>
2020-07-15 17:25:54 -0700

    notification-hubs-test-app-refresh -> notification-hubs-test-app-java
    The updated test application takes the suffix "-java" in order to allow
    room for a future clone of this application, but written in Kotlin.

---
+++ notification-hubs-test-app-java/src/main/java/com/example/notification_hubs_test_app_java/ui/main/SetupViewModel.java
@@ -0,0 +1,107 @@
+package com.example.notification_hubs_test_app_java.ui.main;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.microsoft.windowsazure.messaging.notificationhubs.NotificationHub;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class SetupViewModel extends ViewModel {
+    private final static Pattern sTagPattern = Pattern.compile("^[a-zA-Z0-9_@#\\.:\\-]{1,120}$");
+
+    private String mUnknownText;
+
+    private final MutableLiveData<String> mDeviceToken = new MutableLiveData<String>();
+    private final MutableLiveData<String> mInstallationId = new MutableLiveData<String>();
+    private final MutableLiveData<List<String>> mTags = new MutableLiveData<List<String>>();
+
+    private final MutableLiveData<Boolean> mIsEnabled = new MutableLiveData<Boolean>();
+
+    public SetupViewModel() {
+        mTags.setValue(iterableToList(NotificationHub.getTags()));
+        mIsEnabled.setValue(NotificationHub.isEnabled());
+
+        String pushChannel = NotificationHub.getPushChannel();
+        if (pushChannel != null) {
+            mDeviceToken.setValue(pushChannel);
+        }
+
+        String installationId = NotificationHub.getInstallationId();
+        if (installationId != null) {
+            mInstallationId.setValue(installationId);
+        }
+    }
+
+    public LiveData<String> getDeviceToken() {
+        return mDeviceToken;
+    }
+
+    public void setDeviceToken(String deviceToken) {
+        mDeviceToken.setValue(deviceToken);
+    }
+
+    public LiveData<String> getInstallationId() {
+        return  mInstallationId;
+    }
+
+    public void setInstallationId(String installationId) {
+        NotificationHub.setInstallationId(installationId);
+        mInstallationId.setValue(installationId);
+    }
+
+    public LiveData<Boolean> getIsEnabled() {
+        return mIsEnabled;
+    }
+
+    public void setIsEnabled(boolean b) {
+        NotificationHub.setEnabled(b);
+        mIsEnabled.setValue(NotificationHub.isEnabled());
+    }
+
+    public LiveData<List<String>> getTags() {
+        return mTags;
+    }
+
+    public void addTag(String tag) {
+        if (!isAllowableTag(tag)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (NotificationHub.addTag(tag)) {
+            mTags.postValue(iterableToList(NotificationHub.getTags()));
+        }
+    }
+
+    public void removeTag(String tag) {
+        if (NotificationHub.removeTag(tag)) {
+            mTags.postValue(iterableToList(NotificationHub.getTags()));
+        }
+    }
+
+    public String getUnknownText() {
+        if (mUnknownText == null) {
+            return "";
+        }
+        return mUnknownText;
+    }
+
+    public void setUnknownText(String unknownText) {
+        mUnknownText = unknownText;
+    }
+
+    static <T> List<T> iterableToList(Iterable<T> iterable) {
+        List<T> retval = new ArrayList<T>();
+        for (T entry: iterable) {
+            retval.add(entry);
+        }
+        return retval;
+    }
+
+    static boolean isAllowableTag(String tag) {
+        return sTagPattern.matcher(tag).matches();
+    }
+}
---
+++ notification-hubs-test-app-java/src/main/java/com/example/notification_hubs_test_app_java/ui/main/TagDisplayAdapter.java
@@ -0,0 +1,118 @@
+package com.example.notification_hubs_test_app_java.ui.main;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.notification_hubs_test_app_java.R;
+
+import java.util.List;
+
+public class TagDisplayAdapter extends RecyclerView.Adapter<TagDisplayAdapter.ViewHolder> {
+
+    private List<String> mTags;
+    private final SetupViewModel mViewModel;
+
+    public TagDisplayAdapter(SetupViewModel viewModel) {
+        mViewModel = viewModel;
+    }
+
+    /**
+     * Called when RecyclerView needs a new {@link androidx.recyclerview.widget.RecyclerView.ViewHolder} of the given type to represent
+     * an item.
+     * <p>
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     * <p>
+     * The new ViewHolder will be used to display items of the adapter using
+     * {@link #onBindViewHolder(androidx.recyclerview.widget.RecyclerView.ViewHolder, int, List)}. Since it will be re-used to display
+     * different items in the data set, it is a good idea to cache references to sub views of
+     * the View to avoid unnecessary {@link View#findViewById(int)} calls.
+     *
+     * @param parent   The ViewGroup into which the new View will be added after it is bound to
+     *                 an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new ViewHolder that holds a View of the given view type.
+     * @see #getItemViewType(int)
+     * @see #onBindViewHolder(androidx.recyclerview.widget.RecyclerView.ViewHolder, int)
+     */
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.tag_row_item, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method should
+     * update the contents of the {@link androidx.recyclerview.widget.RecyclerView.ViewHolder#itemView} to reflect the item at the given
+     * position.
+     * <p>
+     * Note that unlike {@link ListView}, RecyclerView will not call this method
+     * again if the position of the item changes in the data set unless the item itself is
+     * invalidated or the new position cannot be determined. For this reason, you should only
+     * use the <code>position</code> parameter while acquiring the related data item inside
+     * this method and should not keep a copy of it. If you need the position of an item later
+     * on (e.g. in a click listener), use {@link androidx.recyclerview.widget.RecyclerView.ViewHolder#getAdapterPosition()} which will
+     * have the updated adapter position.
+     * <p>
+     * Override {@link #onBindViewHolder(androidx.recyclerview.widget.RecyclerView.ViewHolder, int, List)} instead if Adapter can
+     * handle efficient partial bind.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.getTagText().setText(mTags.get(position));
+    }
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in this adapter.
+     */
+    @Override
+    public int getItemCount() {
+        return mTags == null ? 0 : mTags.size();
+    }
+
+    public void setTags(List<String> tags) {
+        mTags = tags;
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView mTagText;
+        private final ImageButton mDeleteButton;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mTagText = (TextView) itemView.findViewById(R.id.tag_entry_value);
+            mDeleteButton = (ImageButton) itemView.findViewById(R.id.tag_delete_button);
+            mDeleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mViewModel.removeTag(mTagText.getText().toString());
+                }
+            });
+        }
+
+        public TextView getTagText() {
+            return mTagText;
+        }
+
+        public ImageButton getDeleteButton() {
+            return mDeleteButton;
+        }
+    }
+}
