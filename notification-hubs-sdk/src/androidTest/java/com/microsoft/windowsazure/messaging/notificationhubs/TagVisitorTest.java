08cb3ef6839c13cd48a09f02a37cc716b5b6f973
Martin Strobel <marstr@microsoft.com>
2020-07-23 18:02:03 -0700

    Use MockSharedPreferences for TagVisitorTest as well.

--- notification-hubs-sdk/src/androidTest/java/com/microsoft/windowsazure/messaging/notificationhubs/TagVisitorTest.java
+++ notification-hubs-sdk/src/androidTest/java/com/microsoft/windowsazure/messaging/notificationhubs/TagVisitorTest.java
@@ -1,24 +1,20 @@
 package com.microsoft.windowsazure.messaging.notificationhubs;
 
-import android.content.Context;
 import androidx.test.filters.SmallTest;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @SmallTest
 public class TagVisitorTest {
-    private Context context = getInstrumentation().getTargetContext();
     private List<String> tagList = new ArrayList<String>();
 
     @Before
@@ -31,7 +27,7 @@ public class TagVisitorTest {
 
     @Test
     public void TagEnricherAddTag () {
-        TagVisitor te = new TagVisitor(context);
+        TagVisitor te = new TagVisitor(new MockSharedPreferences());
 
         assertTrue(te.addTag(tagList.get(0)));
         assertTrue(((HashSet<String>)te.getTags()).contains(tagList.get(0)));
@@ -39,7 +35,7 @@ public class TagVisitorTest {
 
     @Test
     public void TagEnricherAddTags () {
-        TagVisitor te = new TagVisitor(context);
+        TagVisitor te = new TagVisitor(new MockSharedPreferences());
 
         List<String> secondTagList = new ArrayList<String>();
         secondTagList.add("tag4");
@@ -55,7 +51,7 @@ public class TagVisitorTest {
 
     @Test
     public void TagEnricherClearTags () {
-        TagVisitor te = new TagVisitor(context);
+        TagVisitor te = new TagVisitor(new MockSharedPreferences());
 
         assertTrue(te.addTags(tagList));
         assertTrue(((HashSet<String>)te.getTags()).containsAll(tagList));
@@ -65,7 +61,7 @@ public class TagVisitorTest {
 
     @Test
     public void TagEnricherRemoveTag () {
-        TagVisitor te = new TagVisitor(context);
+        TagVisitor te = new TagVisitor(new MockSharedPreferences());
 
         assertTrue(te.addTags(tagList));
         assertTrue(((HashSet<String>)te.getTags()).containsAll(tagList));
@@ -76,7 +72,7 @@ public class TagVisitorTest {
 
     @Test
     public void TagEnricherRemoveTags () {
-        TagVisitor te = new TagVisitor(context);
+        TagVisitor te = new TagVisitor(new MockSharedPreferences());
 
         assertTrue(te.addTags(tagList));
         assertTrue(((HashSet<String>)te.getTags()).containsAll(tagList));
