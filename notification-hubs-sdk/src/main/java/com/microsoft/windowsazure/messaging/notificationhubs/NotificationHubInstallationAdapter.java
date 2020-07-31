aeab96466b40753e5451f29fab35b4d433fa3b33
Martin Strobel <marstr@microsoft.com>
2020-07-30 18:45:03 -0700

    Pull retry responsiblity from Volley, and add it to the NotificationHub adapter itself.

--- notification-hubs-sdk/src/main/java/com/microsoft/windowsazure/messaging/notificationhubs/NotificationHubInstallationAdapter.java
+++ notification-hubs-sdk/src/main/java/com/microsoft/windowsazure/messaging/notificationhubs/NotificationHubInstallationAdapter.java
@@ -2,13 +2,19 @@ package com.microsoft.windowsazure.messaging.notificationhubs;
 
 import android.content.Context;
 
-import com.android.volley.Request;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkError;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Responsible for informing Azure Notification Hubs of changes to when this device should receive
@@ -16,6 +22,8 @@ import java.util.Date;
  */
 public class NotificationHubInstallationAdapter implements InstallationAdapter {
     private static final long DEFAULT_INSTALLATION_EXPIRATION_MILLIS = 1000L * 60L * 60L * 24L * 90L;
+    private static final RetryPolicy sDoNotRetry;
+    private static final Set<Integer> sRetriableStatusCodes;
 
     private final String mHubName;
     private final ConnectionString mConnectionString;
@@ -34,6 +42,17 @@ public class NotificationHubInstallationAdapter implements InstallationAdapter {
         mInstallationExpirationWindow = installationExpirationWindow;
     }
 
+    static {
+        sDoNotRetry = new DefaultRetryPolicy(1000, 0, 1);
+        sRetriableStatusCodes = new HashSet<Integer>();
+        sRetriableStatusCodes.add(500); // Internal Server Error
+        sRetriableStatusCodes.add(503); // Service Unavailable
+        sRetriableStatusCodes.add(504); // Gateway Timeout
+        sRetriableStatusCodes.add(403); // Forbidden (legacy throttling code)
+        sRetriableStatusCodes.add(408); // Client Timeout
+        sRetriableStatusCodes.add(429); // Too Many Requests
+    }
+
     /**
      * Updates a backend with the updated Installation information for this device.
      *
@@ -42,17 +61,7 @@ public class NotificationHubInstallationAdapter implements InstallationAdapter {
     @Override
     public void saveInstallation(final Installation installation, final Listener onInstallationSaved, final ErrorListener onInstallationSaveError) {
         addExpiration(installation);
-
-        Request<?> request;
-
-        try {
-            request = new InstallationPutRequest(mConnectionString, mHubName, installation, onInstallationSaved, onInstallationSaveError);
-        } catch (JSONException e) {
-            onInstallationSaveError.onInstallationSaveError(new IllegalArgumentException("unable to serialize installation to JSON", e));
-            return;
-        }
-
-        mRequestQueue.add(request);
+        new RequestUsher(installation, 3, onInstallationSaved, onInstallationSaveError).submit();
     }
 
     /**
@@ -68,6 +77,80 @@ public class NotificationHubInstallationAdapter implements InstallationAdapter {
         expiration = new Date(expiration.getTime() + mInstallationExpirationWindow);
 
         target.setExpiration(expiration);
+    }
+
+    static boolean isRetriable(VolleyError error) {
+        if (error instanceof NetworkError) {
+            return true;
+        }
+
+        if (error.networkResponse != null ){
+            if(sRetriableStatusCodes.contains(error.networkResponse.statusCode)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Responsible for creating
+     */
+    private class RequestUsher implements Response.ErrorListener, Response.Listener<JSONObject> {
+        private final int mMaxRetries;
+        private int mRetry;
+        private final Installation mInstallation;
+        private final InstallationAdapter.Listener mOnSuccess;
+        private final InstallationAdapter.ErrorListener mOnFailure;
+
+        public RequestUsher(Installation installation, int maxRetries, InstallationAdapter.Listener onSuccess, InstallationAdapter.ErrorListener onFailure) {
+            mMaxRetries = maxRetries;
+            mInstallation = installation;
+            mOnSuccess = onSuccess;
+            mOnFailure = onFailure;
+            mRetry = 0;
+        }
+
+        private void submit() {
+            InstallationPutRequest request = new InstallationPutRequest(
+                    NotificationHubInstallationAdapter.this.mConnectionString,
+                    NotificationHubInstallationAdapter.this.mHubName,
+                    mInstallation,
+                    this,
+                    this);
+
+            request.setRetryPolicy(sDoNotRetry);
+
+            mRequestQueue.add(request);
+        }
+
+        /**
+         * Called when a response is received.
+         *
+         * @param response The JSON Object returned by the server (if any).
+         */
+        @Override
+        public void onResponse(JSONObject response) {
+            mOnSuccess.onInstallationSaved(mInstallation);
+        }
+
+        /**
+         * Callback method that an error has been occurred with the provided error code and optional
+         * user-readable message.
+         *
+         * @param error The reason the Installation was not saved with the backend.
+         */
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            mRetry++;
+            if(!isRetriable(error) || mRetry > mMaxRetries) {
+                // TODO: wrap this error with ours to isolate ourselves from Volley as a dependency.
+                mOnFailure.onInstallationSaveError(error);
+                return;
+            }
+
+            submit();
+        }
     }
 }
 
