963303b6f8aa32f5ca09aef305c3ea3f2de08d8b
Martin Strobel <marstr@microsoft.com>
2022-01-06 11:59:37 -0800

    Export MainActivity so Amazon Launcher doesn't crash

---
+++ notification-hubs-sdk/src/adm/java/com/microsoft/windowsazure/messaging/notificationhubs/NotificationHubExtension.java
@@ -0,0 +1,8 @@
+package com.microsoft.windowsazure.messaging.notificationhubs;
+
+class NotificationHubExtension {
+    public static void fetchPushChannel(final NotificationHub hub)
+    {
+        throw new RuntimeException("Not Implemented");
+    }
+}
